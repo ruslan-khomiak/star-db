6469fde4ffe1da7175164ed26140d03c6da7b038
Ruslan Khomiak <ruslan.khomiak@stfalcon.com>
2019-06-19 12:35:32 +0300

    added something for error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PeoplePage from '../people-page';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
 
 import './app.css';
 
@@ -11,24 +12,45 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
+    hasError: false,
   };
 
-  onPersonSelected = (id) => this.setState({ selectedPerson: id });
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  };
 
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
     return (
       <div>
         <Header/>
-        <RandomPlanet/>
-        <div className='row mb2'>
-          <div className='col-md-6'>
-            <ItemList onItemSelected={ this.onPersonSelected } />
-          </div>
-          <div className='col-md-6'>
-            <PersonDetails personId={ this.state.selectedPerson }/>
-          </div>
+        { planet }
+        <div className='row mb2 button-row'>
+          <button
+            className='toggle-planet btn btn-warning btn-lg'
+            onClick={this.toggleRandomPlanet}
+          >
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
         </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   }
