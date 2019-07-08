c25d69232f70e14d368bdc66fa8d92c7c3de74e5
Ruslan Khomiak <ruslan.khomiak@stfalcon.com>
2019-07-08 16:16:10 +0300

    added routing for pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,6 +10,8 @@ import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './app.css';
 
 export default class App extends Component {
@@ -41,15 +43,22 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={ this.state.swapiService }>
-          <div className="stardb-app">
-            <Header onServiceChange={ this.onServiceChange }/>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={ this.onServiceChange }/>
+              <RandomPlanet/>
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
+              <Route
+                path="/"
+                render={() => <h2>Welcome to StarDB</h2>}
+                exact
+              />
+              <Route path="/people" component={ PeoplePage } />
+              <Route path="/planets" component={ PlanetsPage } />
+              <Route path="/starships" component={ StarshipsPage } />
 
-          </div>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
